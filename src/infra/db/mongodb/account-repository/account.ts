880198eec8fab9c8ff619853186dde8fd7270731
Robsondj <robson.djsilva@gmail.com>
2023-07-11 15:33:49 -0300

    :refactor fixing return with _id

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,7 +6,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add (data: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(data)
+    const result = await accountCollection.insertOne({ ...data })
     const objectId = result.insertedId
     return Object.assign({}, data, { id: objectId.toString() })
   }
