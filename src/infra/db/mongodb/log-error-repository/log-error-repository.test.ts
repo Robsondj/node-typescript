265a3bee7427c74f0436ec0b4bdbb2229b7020b2
Robsondj <robson.djsilva@gmail.com>
2023-07-19 13:59:43 -0300

    feat: add log error mongo repository

---
+++ src/infra/db/mongodb/log-error-repository/log-error-repository.test.ts
@@ -0,0 +1,32 @@
+import { type Collection } from 'mongodb'
+import { type LogErrorRepository } from '../../../../data/interfaces/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogErrorMongoRepository } from './log-error-repository'
+
+let logErrorCollection: Collection
+
+describe('LogError Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? 'localhost')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    logErrorCollection = MongoHelper.getCollection('logError')
+    await logErrorCollection.deleteMany({})
+  })
+
+  const makeSut = (): LogErrorRepository => {
+    return new LogErrorMongoRepository()
+  }
+
+  test('Should insert an log on success', async () => {
+    const sut = makeSut()
+    await sut.add('Stack Error')
+    const count = await logErrorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
---
+++ src/infra/db/mongodb/log-error-repository/log-error-repository.ts
@@ -0,0 +1,12 @@
+import { type LogErrorRepository } from '../../../../data/interfaces/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogErrorMongoRepository implements LogErrorRepository {
+  async add (stack: string): Promise<void> {
+    const logErrorCollection = MongoHelper.getCollection('logError')
+    await logErrorCollection.insertOne({
+      stack,
+      date: new Date()
+    })
+  }
+}
