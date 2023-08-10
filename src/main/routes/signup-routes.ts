e4d2eee9e8684095f586db666e7bd3604b678507
Robsondj <robson.djsilva@gmail.com>
2023-08-10 11:49:19 -0300

    fix: fix signup import

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { type RequestHandler, type Router } from 'express'
 import { adaptRoute } from '../adapter/express-route-adapter'
-import { makeSignUpController } from '../factories/signup'
+import { makeSignUpController } from '../factories/signup/signup'
 
 export default (route: Router): void => {
   route.post('/signup', adaptRoute(makeSignUpController()) as RequestHandler)
