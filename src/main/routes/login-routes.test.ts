a2b53f5c01368c0d4f91d00ba6448f46cdfd7c8f
Robsondj <robson.djsilva@gmail.com>
2023-09-01 16:37:15 -0300

    feat: add login route tests

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,10 +2,13 @@ import request from 'supertest'
 import { setupApp } from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { type Express } from 'express'
+import { hash } from 'bcrypt'
+import { type Collection } from 'mongodb'
 
 let app: Express
+let accountCollection: Collection
 
-describe('SignUp Route', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     app = await setupApp()
     await MongoHelper.connect(process.env.MONGO_URL ?? 'mongodb://localhost:27017/scale-node-api')
@@ -20,14 +23,44 @@ describe('SignUp Route', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return 200 on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
+  describe('SignUp Route', () => {
+    test('Should return 200 on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Robson',
+          email: 'robson@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }).expect(200)
+    })
+  })
+
+  describe('Login Route', () => {
+    test('Should return 200 on success', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
         name: 'Robson',
         email: 'robson@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      }).expect(200)
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'robson@gmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+
+    test('Should return 401 on fail', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'robson@gmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
   })
 })
